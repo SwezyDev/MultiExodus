04a697a0edae42542e2e16d47fc505fa881c65b2
Swezy <109398018+SwezyDev@users.noreply.github.com>
2025-11-23 16:02:19 +0100

    Update app.py

--- multi_exodus/app.py
+++ multi_exodus/app.py
@@ -10,9 +10,18 @@ def title_updater(root): # function to update the window title with wallet count
         root.title(f"MultiExodus - {count} Loaded Wallets | {datetime.now().strftime('%H:%M:%S')}") # update title with wallet count and current time
         time.sleep(1) # Update every second
 
+def center_me(root, width, height): # function to center the window on the screen
+    screen_width = root.winfo_screenwidth() # get screen width
+    screen_height = root.winfo_screenheight() # get screen height
+
+    x = int((screen_width / 2) - (width / 2)) # calculate x position
+    y = int((screen_height / 2) - (height / 2)) # calculate y position
+
+    root.geometry(f"{width}x{height}+{x}+{y}") # set window geometry
+
 def main(): # main function to start the application
     root = customtkinter.CTk(fg_color="#202020") # create the main window
-    root.geometry("1375x700") # set window size
+    center_me(root, 1375, 700) # center the window
     root.resizable(False, False) # disable resizing
     root.iconbitmap("./assets/app.ico") # set window icon
 
