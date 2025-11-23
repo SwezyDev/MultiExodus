58f62cecf70b82fc989ea13757095a0a87184638
Swezy <109398018+SwezyDev@users.noreply.github.com>
2025-11-23 19:55:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from multi_exodus import main
+from multi_exodus import main, constants
+import ctypes
+import os
 
 if __name__ == '__main__': 
     print("Welcome to MultiExodus Wallet Manager!")
@@ -8,5 +10,13 @@ if __name__ == '__main__':
     print("-------------------------------------")
     print("Contact: https://t.me/Swezy")
     # print some info to the console about the project :)
+
+    if not constants.MULTI_WALLET_DIR.exists(): # check if the MultiExodus directory exists
+        constants.MULTI_WALLET_DIR.mkdir(parents=True, exist_ok=True) # create the directory if it doesnt exist
+
+    if not constants.EXODUS_DIR.exists(): # check if the Exodus directory exists
+        ctypes.windll.user32.MessageBoxW(0, f"Exodus directory not found! Please install Exodus wallet first.", "MultiExodus", 0x03 | 0x30) # show backup confirmation dialog
+        os.system("start https://www.exodus.com/download/") # open Exodus download page
+        os._exit(0) # exit the application if Exodus is not installed
 
     main() # Start the MultiExodus application
